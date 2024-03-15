15e2f006f93bf709b81f1d6d2e38b758fe832baf
JerryL0923 <jerrylamemail@gmail.com>
2024-03-15 13:02:30 -0700

    somewhat working model

--- RandomPermutation.java
+++ RandomPermutation.java
@@ -1,17 +1,37 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class RandomPermutation {
-  public static next() {
-    int[] defaultNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
-    int[] finalNumbers = new int[10]; 
-    Random random = new Random();
+    public static void main(String[] args) {
 
-    for (int i = 0; i < defaultNumbers.length; i++){ 
-      int randomInt = random.nextInt(10) + 1;
-      if (finalNumbers[i] != 0) {
-        finalNumbers[i] = defaultNumbers[randomInt]; 
+      int p[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+      int r[] = new int[p.length];
+
+      for (int i = 0; i < p.length; i++) {
+        int index = next(p);
+        r[i] = p[index];
+        p = removeElement(p, index);
       }
-      System.out.println(java.util.Arrays.toString(r));
+
+      System.out.println(Arrays.toString(r));
     }
+
+    
+    
+    
+    
+    
+    public static int next(int p[]) {
+      return new Random().nextInt(p.length);
+    }
+
+    public static int[] removeElement(int[] array, int index) {
+      int[] newArray = new int[array.length];
+      System.arraycopy(array, 0, newArray, 0, index);
+      System.arraycopy(array, index + 1, newArray, index, array.length - index - 1);
+      newArray[newArray.length - 1] = 0; // Add this line to set the last element to 0
+      System.out.println(java.util.Arrays.toString(newArray));
+      return newArray;
   }
 }
